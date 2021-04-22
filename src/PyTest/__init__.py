538a0d99ef4072af4c674f91c21183dd6710f699
quervernetzt <info@quervernetzt.de>
2021-04-22 18:13:39 +0200

    enhance azure function request processing

--- src/PyTest/__init__.py
+++ src/PyTest/__init__.py
@@ -1,15 +1,30 @@
 import logging
-
 import azure.functions as func
 import requests
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Azure Function has been triggered...")
 
-    r = requests.get('https://api.github.com/events')
+    response: str = ""
+    status_code: int = -1
+
+    request_url: str = req.params.get("url")
+    if not request_url:
+        response = "Please specify a url..."
+        status_code = 400
+    else:
+        try:
+            request_response: requests.Response = requests.get(request_url)
+            response = request_response.content
+            status_code = 200
+        except Exception as e:
+            logging.error("Request failed...")
+            response = e.args[0]
+            # should actually be more granular but this is fine for demonstration purposes
+            status_code = 500
 
     return func.HttpResponse(
-             r.content,
-             status_code=200
-        )
+        response,
+        status_code=status_code
+    )
